818562a35940d97177428ac5cdb83fda9df29312
Christie Mealo <christiemealo@gmail.com>
2025-07-23 21:20:46 -0400

    added codea variable

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let code1 = 7 + 3;
+// evalutates to 10
 
 let code2 = 100 / 2 - 10;
+// evaluates to 40
 
 let code3 = 40 - (3 % 2);
+// evaluates to 39
 
 let message = `"The vault has been secured. The combination is:`;
+
+let codeA = `${code1}-${code2}-${code3}`;
